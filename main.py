3ffef042067db68ce0a4d8cfba4b721adca66109
Haider <butt6pk60@hotmail.com>
2022-07-19 15:03:33 +0500

    CHANGE SQLITE TO POSTGRE SQL DATABASE

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, flash,g
 from werkzeug.exceptions import abort
 from sqlalchemy import Table, Column, Integer, ForeignKey
@@ -17,7 +19,7 @@ from functools import wraps
 
 Base = declarative_base()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -32,7 +34,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
